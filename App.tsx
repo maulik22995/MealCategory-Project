172f34aa9c111f2e8f4576eea48c15a87b9d2eab
Maulik Togadiya <maulik22995@gmail.com>
2023-07-23 12:44:54 +0530

    intialize code

---
+++ App.tsx
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import CategoriesList from './screens/CategoriesList';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MyMealOverViewMeal from './screens/MyMealOverView';
+import MealDetailScreen from './screens/MealDetailsScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import FavouriteScreen from './screens/FavouriteScreen';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: '#351401'},
+        headerTintColor: 'white',
+        sceneContainerStyle: {backgroundColor: '#3f2f25'}
+        drawerContetS,
+      }}>
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesList}
+        options={{title: 'All Category'}}></Drawer.Screen>
+      <Drawer.Screen
+        name="Favourites"
+        component={FavouriteScreen}></Drawer.Screen>
+    </Drawer.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <StatusBar barStyle={'light-content'} backgroundColor={'#351401'} />
+      <View style={{flex: 1, backgroundColor: '#24180f'}}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: {backgroundColor: '#351401'},
+              headerTintColor: 'white',
+              contentStyle: {backgroundColor: '#3f2f25'},
+            }}>
+            <Stack.Screen
+              name="Drawer"
+              component={DrawerNavigator}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="MealOverview"
+              component={MyMealOverViewMeal}
+              // options={({route, navigation}) => {
+              //   const catId = route.params.categoryID;
+              //   return {
+              //     title: catId,
+              //   };
+              // }}
+            />
+            <Stack.Screen
+              name="MealDetails"
+              component={MealDetailScreen}
+              options={{
+                title: 'About the meal',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
